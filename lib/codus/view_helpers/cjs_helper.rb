4905d5581f2639ecea946b091c9ae4b94edbceb3
Vinícius Oyama <vinicius.oyama@gmail.com>
2013-05-27 09:19:07 -0300

    code reorganized

--- lib/codus/view_helpers/cjs_helper.rb
+++ lib/codus/view_helpers/cjs_helper.rb
@@ -1,72 +1,10 @@
+require "codus/view_helpers/cjs_helper/cjs_call_generator"
+
 module Codus
   module ViewHelpers
     module CjsHelper
-      class CjsOnloadCallGenerator
-        def initialize(controller, action, options = {})
-          @options = merge_options_with_defaults(options)
-          @parent_namespace = "#{@options[:app_name]}.#{controller.parameterize('.')}"
-          @last_namespace = action.parameterize(".")
-          @fullnamespace = "#{@parent_namespace}.#{@last_namespace}"
-          
-        end
-
-        def merge_options_with_defaults(options)
-          default_options = { :app_name => "", 
-            :onload_method_name => "",
-            :method_names_mapper => {
-              :create => :new,
-              :update => :edit
-            }
-          }
-          mapping_options = options.delete(:method_names_mapper)
-          default_options[:method_names_mapper].merge!(mapping_options) unless mapping_options.nil? 
-          default_options.merge(options)
-        end
-
-        def generate_cjs_calls
-          namespaces_to_call_onload = get_all_namespaces_from_full_namespace
-          onload_calls_definitions = append_onload_method_to_namespaces(namespaces_to_call_onload)
-          conditional_callings = generate_conditional_function_calling_for_namespaces(onload_calls_definitions)
-          conditional_callings.join("\n").html_safe
-        end
-
-        def get_all_namespaces_from_full_namespace
-          namespaces = []
-          @fullnamespace.split(".").each do |actual_namespace| 
-            if namespaces.empty?
-              namespaces << actual_namespace
-            else
-              namespaces << ("#{namespaces.last}.#{actual_namespace}")
-            end
-          end
-          if @options[:method_names_mapper].has_key?(@fullnamespace.split(".").last.to_sym)
-            namespaces << ("#{@parent_namespace}.#{@options[:method_names_mapper][@last_namespace.to_sym]}")
-          end
-          namespaces
-        end
-
-        def append_onload_method_to_namespaces(namespaces = [])
-          return namespaces if @options[:onload_method_name].nil? || @options[:onload_method_name] == ""
-          namespaces.map do |namespace_to_call_onload|
-            "#{namespace_to_call_onload}.#{@options[:onload_method_name]}"
-          end
-        end
-
-        def generate_conditional_function_calling_for_namespaces(namespaces = [])
-          namespaces.map do |call_definition| 
-            "
-              if (cJS.isDefined(\"#{call_definition}\")) {
-                cJS.call(\"#{call_definition}\");
-              }
-            "
-          end
-        end
-
-        
-      end
-
       def load_cjs(options = {})
-        call_generator = CjsOnloadCallGenerator.new(params[:controller], params[:action], options)
+        call_generator = CjsCallGenerator.new(params[:controller], params[:action], options)
         javascript_ready { call_generator.generate_cjs_calls() }.html_safe
       end
     end
---
+++ spec/cjs/view_helpers/cjs_helper/cjs_call_generator_spec.rb
@@ -0,0 +1,102 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Codus::ViewHelpers::CjsHelper::CjsCallGenerator do 
+  subject { Codus::ViewHelpers::CjsHelper::CjsCallGenerator.new("ns2", "ns3", {:app_name => "ns1"}) }
+  describe "generate_cjs_calls" do 
+    specify do
+      namespaces_array = ["ns1", "ns1.ns2", "ns1.ns2.ns3"]
+      namespaces_array_with_method = ["ns1.load", "ns1.ns2.load", "ns1.ns2.ns3.load", "ns1.ns2.ns3.load"]
+      conditional_callings = [
+          "\n          if (cJS.isDefined(\"ns2\")) {\n            cJS.call(\"ns2\");\n          }\n        ",
+          "\n          if (cJS.isDefined(\"ns4.ng5\")) {\n            cJS.call(\"ns4.ng5\");\n          }\n        "]
+      subject.should_receive(:get_all_namespaces_from_full_namespace).and_return(namespaces_array)
+      subject.should_receive(:append_onload_method_to_namespaces).with(namespaces_array).and_return(namespaces_array_with_method)
+      subject.should_receive(:generate_conditional_function_calling_for_namespaces).with(namespaces_array_with_method).and_return(conditional_callings)
+      subject.generate_cjs_calls.should be == "\n          if (cJS.isDefined(\"ns2\")) {\n            cJS.call(\"ns2\");\n          }\n        \n\n          if (cJS.isDefined(\"ns4.ng5\")) {\n            cJS.call(\"ns4.ng5\");\n          }\n        "
+    end
+  end
+
+  describe "get_all_namespaces_from_full_namespace" do
+    specify { subject.get_all_namespaces_from_full_namespace.should be == %w(ns1 ns1.ns2 ns1.ns2.ns3)}
+
+    it "should return mapped ns names" do 
+      generator = Codus::ViewHelpers::CjsHelper::CjsCallGenerator.new("ns2", "create", {:app_name => "ns1", :method_names_mapper => {:create => "novo"}})
+      generator.get_all_namespaces_from_full_namespace().should be == %w(ns1 ns1.ns2 ns1.ns2.create ns1.ns2.novo)
+      generator = Codus::ViewHelpers::CjsHelper::CjsCallGenerator.new("ns2", "edit", {:app_name => "ns1", :method_names_mapper => {:create => "novo"}})
+      generator.get_all_namespaces_from_full_namespace().should be == %w(ns1 ns1.ns2 ns1.ns2.edit)
+    
+    end
+  end
+
+  describe "append_onload_method_to_namespaces" do 
+    context "empty method name" do 
+      specify { subject.append_onload_method_to_namespaces(["ns2", "ns4.ng5"]).should be == ["ns2", "ns4.ng5"] }
+    end
+
+    context "method name not empty" do 
+      specify do 
+        subject.instance_eval { @options[:onload_method_name] = "onload_method"}
+        subject.append_onload_method_to_namespaces(["ns2", "ns4.ng5"]).should be == ["ns2.onload_method", "ns4.ng5.onload_method"] 
+      end
+    end
+  end
+
+  describe "generate_conditional_function_calling_for_namespaces" do 
+    specify do 
+      subject.generate_conditional_function_calling_for_namespaces(["ns2", "ns4.ng5"]).should be == [
+        "\n              if (cJS.isDefined(\"ns2\")) {\n                cJS.call(\"ns2\");\n              }\n            ", 
+        "\n              if (cJS.isDefined(\"ns4.ng5\")) {\n                cJS.call(\"ns4.ng5\");\n              }\n            "
+      ]
+    end
+  end
+
+  describe "merge_options_with_defaults" do 
+    describe "default_values" do 
+      specify { subject.merge_options_with_defaults({})[:app_name].should be == ""}
+      specify { subject.merge_options_with_defaults({})[:onload_method_name].should be == ""}
+      specify { subject.merge_options_with_defaults({})[:method_names_mapper].should include({
+          :create => :new,
+          :update => :edit
+        }) 
+      }
+    end
+
+    context "merging parameters" do 
+      specify do
+        subject.merge_options_with_defaults({:app_name => "NOMETESTE"})[:app_name].should be == "NOMETESTE"
+      end
+
+      specify do 
+        subject.merge_options_with_defaults({:onload_method_name => "NOMETESTE"})[:onload_method_name].should be == "NOMETESTE"
+      end
+
+      specify do 
+        subject.merge_options_with_defaults({:method_names_mapper => {:rota => :novo_nome}})[:method_names_mapper].should include({
+          :create => :new,
+          :update => :edit,
+          :rota => :novo_nome
+        }) 
+      end
+
+      specify do 
+        customized_options =  subject.merge_options_with_defaults({
+          :app_name => "NOMEAPPTESTE",
+          :onload_method_name => "NOMELOADTESTE",
+          :method_names_mapper => {
+            :rota => :novo_nome,
+            :create => :createpersonalizado
+          }
+        })
+
+        customized_options[:app_name].should be == "NOMEAPPTESTE"
+        customized_options[:onload_method_name].should be == "NOMELOADTESTE"
+        customized_options[:method_names_mapper].should include({
+          :update => :edit,
+          :rota => :novo_nome,
+          :create => :createpersonalizado
+        }) 
+      end
+    end
+  end
+end
